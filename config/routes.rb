547b1830ec441c91ce4c564fa3ba192ec57baabd
john_lee <jjlee099@gmail.com>
2017-02-27 14:29:35 -0800

    Added MVC for users, including authentication/authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'pages/secret'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Add a root route if you don't have one...
+  # We can use users#new for now, or replace this with the controller and action you want to be the site root:
+  root to: 'users#new'
+
+  # sign up page with form:
+  get 'users/new' => 'users#new', as: :new_user
+
+  # create (post) action for when sign up form is submitted:
+  post 'users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
+
 end
