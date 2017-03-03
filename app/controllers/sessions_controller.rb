23d82419c258d56c7cb86051a73e0a21511d0be6
john_lee <jjlee099@gmail.com>
2017-03-02 16:49:50 -0800

    PRettified Login and SIgn up Pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:login][:username].downcase)
+    user = User.find_by(username: params[:username].downcase)
 
-    if user && user.authenticate(params[:login][:password])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id.to_s
       redirect_to posts_path, notice: 'Successfully logged in!'
     else
